67662bc712b67eed2a155274bec6a35bd66992f7
sandro-defender <defender@you.ge>
2025-03-24 23:33:48 +0400

    Delete js directory

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-    REPO: 'sandro-defender/tip',
-    TOKEN: process.env.GITHUB_TOKEN || '', // Будет заполняться через переменные окружения
-    TIMEZONE_OFFSET: 4,
-    PASSWORD: process.env.STATS_PASSWORD || '4545' // Будет заполняться через переменные окружения
-};
-
-export default CONFIG; 
