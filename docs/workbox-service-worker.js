92d2c22f4523d0ef3d314c88cf4ab5e430778608
sandro-defender <defender@you.ge>
2025-04-03 22:05:38 +0400

    sa

--- docs/workbox-service-worker.js
+++ docs/workbox-service-worker.js
@@ -2,7 +2,7 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
 // Версия для управления кэшем
-const VERSION = "v1.1.5";
+const VERSION = "v1.1.6";
 
 // Список URL-адресов для исключения из кэша
 const EXCLUDED_URLS = [
