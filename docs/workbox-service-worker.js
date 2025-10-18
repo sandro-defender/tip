e44af14462a9c67848e5e2c0395773dfd9a39829
sandro-defender <defender@you.ge>
2025-10-18 05:56:51 +0400

    memo

--- docs/workbox-service-worker.js
+++ docs/workbox-service-worker.js
@@ -75,7 +75,7 @@ Purpose: Service Worker for PWA functionality with push notifications and cachin
    { url: '/shift', revision: VERSION },
    { url: '/salary', revision: VERSION },
    { url: '/bar', revision: VERSION },
- 
+   { url: '/memo', revision: VERSION },
  
    // CDN-ფაილები
    { url: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js', revision: '5.1.3' },
